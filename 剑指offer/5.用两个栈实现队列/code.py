07fd58ede554909b81ae4e046d80d03098de9b60
forthlsss <hsheng96@163.com>
2020-01-02 14:26:40 +0800

    Create code.py

---
+++ 剑指offer/5.用两个栈实现队列/code.py
@@ -0,0 +1,14 @@
+class Solution:
+    #使用self，把stack1,2当作class的两个属性，pop和push成为两个方法
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+    def push(self,node):
+        self.stack1.append(node)
+    def pop(self):
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
